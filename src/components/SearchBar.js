44caab75aeb81a8e9b92e4c6787171e5ab2f8968
Kaustubh <saikaustubh10@gmail.com>
2021-02-27 13:19:58 +0530

    fixed getting user input

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,26 @@
 import React from 'react';
 
 class SearchBar extends React.Component{
+
+    state = { term: '' };
+    // if you get state not defined just use arrow function to solve the problem
+    onFormSubmit = (event) => {
+        event.preventDefault();
+        // to access props in 
+        this.props.onSubmit(this.state.term);
+    }
+
     render(){
         return (
             <div className="ui segment">
-                <form className="ui form">
+                <form onSubmit={this.onFormSubmit} className="ui form">
                     <div className="field">
-                        <label><i class="search icon"></i> Image Search</label>
-                        <input type="text"></input>
+                        <label><i className="search icon"></i> Image Search</label>
+                        <input type="text" 
+                            value={this.state.term} 
+                            onChange={(e) => this.setState({ term: e.target.value })} 
+                                
+                        />
                     </div>
                 </form>
             </div>
