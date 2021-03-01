cde95f9dbeec66babfd5287665e3b36e508fefbe
Kaustubh <saikaustubh10@gmail.com>
2021-03-01 10:53:25 +0530

    completed code

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export default axios.create({
     baseURL: 'https://api.unsplash.com',
     headers: {
-        Authorization: 'Client-ID ${process.env}',
+        Authorization: 'Client-ID H1lFBK01dNYSPQuN5nlAHr2UGV00zMwwpVuZg_ZlFCw',
     },
 });
 
