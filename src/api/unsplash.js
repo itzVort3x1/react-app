166b6dc05b9286e84191b8f602108d2757aae48f
Kaustubh <saikaustubh10@gmail.com>
2021-03-01 10:54:39 +0530

    minor change

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export default axios.create({
     baseURL: 'https://api.unsplash.com',
     headers: {
-        Authorization: 'Client-ID H1lFBK01dNYSPQuN5nlAHr2UGV00zMwwpVuZg_ZlFCw',
+        Authorization: 'Client-ID ACCESS_KEY',
     },
 });
 
